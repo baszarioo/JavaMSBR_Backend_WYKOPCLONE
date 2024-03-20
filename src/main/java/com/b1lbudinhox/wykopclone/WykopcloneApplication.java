2937c563a8228615fcd534be3a11efdb51564ee3
baszarioo <113721856+baszarioo@users.noreply.github.com>
2024-03-20 01:23:41 +0100

    init commit #1

---
+++ src/main/java/com/b1lbudinhox/wykopclone/WykopcloneApplication.java
@@ -0,0 +1,13 @@
+package com.b1lbudinhox.wykopclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WykopcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WykopcloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b1lbudinhox/wykopclone/WykopcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1lbudinhox.wykopclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WykopcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
