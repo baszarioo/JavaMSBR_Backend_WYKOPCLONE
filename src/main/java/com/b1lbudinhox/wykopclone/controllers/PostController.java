e99c2996ed18fff2f8fb0b35f0e050aa35b23024
baszarioo <113721856+baszarioo@users.noreply.github.com>
2024-03-20 20:22:43 +0100

    init: repositories for modules 🖼
    TODO: services, posts dto.

---
+++ src/main/java/com/b1lbudinhox/wykopclone/controllers/PostController.java
@@ -0,0 +1,13 @@
+package com.b1lbudinhox.wykopclone.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/posts")
+@AllArgsConstructor
+public class PostController {
+//    private final PostService postService;
+    //TODO: mappings.
+}
---
+++ src/main/java/com/b1lbudinhox/wykopclone/repositories/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.b1lbudinhox.wykopclone.repositories;
+
+import com.b1lbudinhox.wykopclone.models.Comment;
+import com.b1lbudinhox.wykopclone.models.Post;
+import com.b1lbudinhox.wykopclone.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
+    List<Comment> findAllByUser(User user);
+}
