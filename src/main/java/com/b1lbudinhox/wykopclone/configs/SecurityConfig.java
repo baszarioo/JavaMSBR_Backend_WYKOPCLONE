7b5ec225b53ef02532ddf27de0de2e35d58891fd
ja669nle2bil <jakubbilinski.info@gmail.com>
2024-03-30 19:46:08 +0100

    Fixing securityConfig, improv securitychain

--- src/main/java/com/b1lbudinhox/wykopclone/configs/SecurityConfig.java
+++ src/main/java/com/b1lbudinhox/wykopclone/configs/SecurityConfig.java
@@ -1,19 +1,23 @@
 package com.b1lbudinhox.wykopclone.configs;
 
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.security.interfaces.RSAPrivateKey;
@@ -34,31 +38,36 @@ public class SecurityConfig {
     }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.cors().and()
-                .csrf().disable()
-                .authorizeHttpRequests("/api/auth/**")
-                .permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/thread")
-                .permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/posts/")
-                .permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/posts/**")
-                .permitAll()
-                .requestMatchers("/v2/api-docs",
-                        "/configuration/ui",
-                        "/swagger-resources/**",
-                        "/configuration/security",
-                        "/swagger-ui.html",
-                        "/webjars/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated())
-        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+        System.out.println("SecurityFilterChain");
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.cors(AbstractHttpConfigurer::disable);
+        httpSecurity
+                .authorizeHttpRequests(request ->
+                    request.requestMatchers("/api/auth/**")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/thread")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/posts/")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/posts/**")
+                    .permitAll()
+                    .requestMatchers("/v2/api-docs",
+                            "/configuration/ui",
+                            "/swagger-resources/**",
+                            "/configuration/security",
+                            "/swagger-ui.html",
+                            "/webjars/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+//            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt) //dpc
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(exceptions -> exceptions
                         .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
-                        .accessDeniedHandler(new BearerTokenAccessDeniedHanlder())
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
                 ).build();
+        return httpSecurity.build();
     }
     @Bean
     PasswordEncoder passwordEncoder() {
